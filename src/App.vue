ca1e8c3c4b55d3b00bb046d4fa6c7260d3fc3c85
SpreeZ00 <admin@spreez.me>
2023-03-01 16:49:29 +0100

    New Design

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,75 @@
 <template>
-      <nav class="sticky top-0 z-50 border-b border-slate-900 dark:border-gray-100">
-        <ul class="nav">
-          <li><RouterLink class="navItem" to="/">Start</RouterLink></li>
-          <li><a class="navItem" href="#about">Über mich</a></li>
-          <li><a class="navItem" href="#contact">Kontakt</a></li>
-          <button class="absolute right-4 top-4 navItem" @click="toggleDarkmode">Toggle Darkmode</button>
-        </ul>
-      </nav>
 
-  <RouterView />
+  <header class="h-screen font-bold flex justify-center relative bg-slate-900 text-white overflow-hidden" @mousemove="handleMouseMove">
+    <img id="scatter" class="w-full bg-cover" src="/polygon-scatter.svg" />
+    <div class="text-left absolute top-1/2 -translate-y-1/2 sm:top-2/3 sm:-translate-y-2/3 font-mono typewriter">
+      <h1 class="text-4xl sm:text-6xl md:text-8xl">Lars-Niklas Rose</h1>
+      <br />
+      <br />
+      <h2 class="text-lg sm:text-2xl md:text-4xl">Web Developer</h2>
+      <h2 class="text-lg sm:text-2xl md:text-4xl">Business Information Student</h2>
+    </div>
+    
+  </header>
+
+  <main class="px-4 py-16 bg-slate-900 flex flex-col gap-4 text-white">
+        <section>
+            <h1 class="text-3xl font-bold text-gray-300">Willkommen auf meiner Portfolio-Website!</h1>
+            <p class="text-md my-3 text-gray-200">Ich studiere aktuell Wirtschaftsinformatik und beschäftige mich intensiv mit der Webentwicklung. Ich bin begeistert von der ständigen Veränderung und Weiterentwicklung der Technologien im Web und bin bestrebt, mein Wissen und meine Fähigkeiten ständig zu erweitern.</p>
+            <p class="text-md my-3 text-gray-200">Auf dieser Website möchte ich meine Projekte und Fähigkeiten präsentieren, die ich während meiner Studienzeit erworben habe. Von der Entwicklung von Websites bis hin zur Programmierung von Anwendungen - ich bin bestrebt, meine Fähigkeiten ständig zu verbessern und anspruchsvolle Projekte zu übernehmen.</p>
+            <p class="text-md my-3 text-gray-200">Schauen Sie sich gerne um und lernen Sie mehr über mich und meine Arbeiten kennen. Ich freue mich auf Ihre Kontaktaufnahme!</p>
+        </section>
+        <section>
+            <h3 class="text-xl font-bold text-gray-300">Kurzer Überblick</h3>
+                <p class="text-md my-3 text-gray-200">Ich bin ein ambitionierter Student im Bereich Wirtschaftsinformatik. Meine Interessen umfassen neue Technologien, Programmierung und das Entwerfen von benutzerfreundlichen Websites. Ich spreche fließend Deutsch und Englisch und habe Grundkenntnisse in Spanisch.</p>
+                <p class="text-md my-3 text-gray-200">In meiner Freizeit lese ich gerne technische Artikel und lerne neue Programmiersprachen. Des Weiteren probiere ich gerne neue Frameworks aus. Ich bin auch ein begeisterter Sportler und genieße es, Zeit mit Freunden und Familie zu verbringen.</p>
+                <p class="text-md my-3 text-gray-200">Ich bin ständig auf der Suche nach neuen Herausforderungen und Möglichkeiten, meine Fähigkeiten zu verbessern. Ich bin überzeugt davon, dass harte Arbeit und ständige Lernbereitschaft der Schlüssel zum Erfolg sind.</p>
+        </section>
+        <section>
+            <h2 class="text-2xl font-bold text-gray-300 mb-6">Meine Projekte</h2>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div class="bg-gray-200 rounded-lg shadow-md p-6 relative overflow-hidden hover:border-blue-700 border border-transparent">
+                    <h3 class="text-xl font-bold mb-4 text-gray-800">Vehicle-Platform</h3>
+                    <p class="text-gray-700 mb-4">Webanwendung zur Erfassung von Fahrzeugen und Statistiken über gefahrene Kilometer</p>
+                    <a class="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors duration-300" href="https://github.com/SpreeZ00/vehicle-platform" target="_blank" rel="noreferrer noopener">Mehr erfahren</a>
+                    <div class="flex items-center justify-center absolute overflow-hidden top-0 right-0 bg-blue-500 w-8 h-8 rounded-bl-full">
+                        <div class="-mt-1 -mr-1 text-white">→</div>
+                    </div>
+                </div>
+                <div class="bg-gray-200 rounded-lg shadow-md p-6 relative overflow-hidden hover:border-blue-700 border border-transparent">
+                    <h3 class="text-xl font-bold mb-4 text-gray-800">Portfolio-Website</h3>
+                    <p class="text-gray-800 mb-4">Webseite zur Präsentation meiner Arbeiten und Fähigkeiten als Webentwickler</p>
+                    <a class="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors duration-300" href="https://github.com/SpreeZ00/my-portfolio" target="_blank" rel="noreferrer noopener">Mehr erfahren</a>
+                    <div class="flex items-center justify-center absolute overflow-hidden top-0 right-0 bg-blue-500 w-8 h-8 rounded-bl-full">
+                        <div class="-mt-1 -mr-1 text-white">→</div>
+                    </div>
+                </div>
+                <div class="bg-gray-200 rounded-lg shadow-md p-6 relative overflow-hidden hover:border-blue-700 border border-transparent">
+                    <h3 class="text-xl font-bold mb-4 text-gray-800">Quiz-App</h3>
+                    <p class="text-gray-800 mb-4">Interaktive Webanwendung zur Durchführung von Quizfragen mit verschiedenen Themen</p>
+                    <a class="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors duration-300" href="https://github.com/SpreeZ00/quiz-app" target="_blank" rel="noreferrer noopener">Mehr erfahren</a>
+                    <div class="flex items-center justify-center absolute overflow-hidden top-0 right-0 bg-blue-500 w-8 h-8 rounded-bl-full">
+                        <div class="-mt-1 -mr-1 text-white">→</div>
+                    </div>
+                </div>
+            </div>
+            <p class="pt-4">Weitere Projekte findest du auf meinem <a class="underline underline-offset-2" href="https://github.com/SpreeZ00" target="_blank" rel="noreferrer noopener">Github</a>-Profil.</p>
+        </section>
+        <section id="about">
+            <h2 class="text-2xl font-bold text-gray-300">Über mich</h2>
+            <p class="text-md my-3 text-gray-200">Ich bin ein leidenschaftlicher Webentwickler, der immer auf der Suche nach neuen Herausforderungen ist. Als Student habe ich eine starke Leidenschaft für Technologie und Programmierung entwickelt, die mich dazu inspiriert hat, eine Karriere in diesem Bereich zu verfolgen.</p>
+            <p class="text-md my-3 text-gray-200">Meine Fähigkeiten umfassen JavaScript, TypeScript, NodeJS, Java, Python, Git, SQL, WordPress, TailwindCSS, Vue3, Nuxt3, React, Prisma, Responsive Design, und ich bin immer bereit, neue Fähigkeiten zu erlernen und mein Wissen zu erweitern. Ich habe an verschiedenen Projekten gearbeitet, die meine Fähigkeiten unter Beweis gestellt haben, und ich bin stolz darauf, was ich bisher erreicht habe.</p>
+            <p class="text-md my-3 text-gray-200">In meiner Freizeit genieße ich es, neue Technologien zu erkunden und an persönlichen Projekten zu arbeiten. Ich bin auch ein begeisterter Sportler und verbringe gerne Zeit mit Freunden und Familie.</p>
+            <p class="text-md my-3 text-gray-200">Ich bin ständig auf der Suche nach neuen Möglichkeiten, um meine Fähigkeiten zu verbessern und mich in der Branche zu etablieren. Ich bin überzeugt davon, dass harte Arbeit und ständige Lernbereitschaft der Schlüssel zum Erfolg sind.</p>
+            <p class="text-md my-3 text-gray-200">Lassen Sie uns zusammenarbeiten und sehen, was wir erreichen können. Kontaktieren Sie mich, um mehr zu erfahren.</p>
+        </section>
+        <section id="contact">
+            <h2 class="text-2xl font-bold text-gray-300">Kontakt</h2>
+            <p class="text-md my-3 text-gray-200">Wenn Sie mehr über meine Arbeit erfahren möchten oder mich für ein Projekt kontaktieren möchten, können Sie mich über meine <a class="underline underline-offset-2" href="mailto:info@larsrose.com">E-Mail-Adresse</a> oder über einen meiner Social-Media Kanäle kontaktieren. Ich werde so schnell wie möglich antworten.</p>
+            <a href="https://linkedin.com/in/lars-niklas-rose-0ba195188" target="_blank" rel="noreferrer noopener"><v-icon name="co-linkedin" scale="2.3" fill="#0a66c2" /></a>
+            <a class="inline-block" href="https://github.com/SpreeZ00" target="_blank" rel="noreferrer noopener"><v-icon name="co-github" scale="2.3" fill="#fff" /></a>
+        </section>
+    </main>
 
   <footer class="bg-gray-400 dark:bg-slate-600 p-4">
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 text-center">
@@ -35,24 +96,15 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import { RouterLink, RouterView } from 'vue-router'
 
-onMounted(() => {
-  if(localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark')
-  } else {
-    document.documentElement.classList.remove('dark')
-  }
-})
+function handleMouseMove(e: MouseEvent) {
+  const x = e.clientX / window.innerWidth
+  const y = e.clientY / window.innerHeight
 
-function toggleDarkmode() {
-  if(document.documentElement.classList.contains('dark')) {
-    localStorage.theme = 'light'
-    document.documentElement.classList.remove('dark')
-  } else {
-    localStorage.theme = 'dark'
-    document.documentElement.classList.add('dark')
-  }
+  const scatter: HTMLImageElement | null = document.querySelector('#scatter');
+  if (!scatter) return;
+  scatter.style.transform = `translate(${x * 50}px, ${y * 50}px)`
+  
 }
+
 </script>
